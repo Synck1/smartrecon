9bed3b7acf072adf2a198c120a52081eb02e2eee
Rafael Soares Martins <75044592+Synck1@users.noreply.github.com>
2025-06-17 18:22:14 -0300

    Update main.go
    Removendo Comentários

--- main.go
+++ main.go
@@ -8,14 +8,11 @@ import (
 )
 
 func main() {
-	// 🔥 Carrega config
 	cfg, err := config.LoadConfig("config/config.yaml")
 	if err != nil {
 		log.Fatal("Erro carregando configuração:", err)
 	}
 
 	fmt.Println("[*] SmartRecon iniciado...")
-
-	// 🚀 Passa a config pro recon
 	smartrecon.Run(cfg)
 }
